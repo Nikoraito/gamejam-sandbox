0e4af4637b4af9aa28f5a66ce6cb6a3955f97d51
Nikoraito <nikoraito@gmail.com>
2018-06-10 17:50:17 -0400

    Created Project
    Created the mother project

---
+++ desktop/src/net/nikoraito/gjsb1/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package net.nikoraito.gjsb1.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import net.nikoraito.gjsb1.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
